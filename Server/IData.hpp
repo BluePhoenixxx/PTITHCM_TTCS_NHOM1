219ae8eef922f385581492e18291d3fede2170a9
dinhquoctoan <dinhquoctoan200103@gmail.com>
2024-03-28 15:42:03 +0700

    thêm ghi chú

--- Server/IData.hpp
+++ Server/IData.hpp
@@ -8,11 +8,12 @@ using namespace std;
 
 class IData{
     public:
-        virtual ~IData() = default;
-        virtual void dump() const = 0;
-        virtual string getData() const = 0;
-        virtual shared_ptr<IData> cloneObj(const vector<char> &data) = 0;
-        virtual void feedObj(const vector<char> &data) = 0;
+        //Các phương thức trừu tượng
+        virtual ~IData() = default; //hủy đối tượng
+        virtual void dump() const = 0; //in đối tượng 
+        virtual string getData() const = 0; //getter
+        virtual shared_ptr<IData> cloneObj(const vector<char> &data) = 0; //tạo bản sao từ đối tượng đầu vào
+        virtual void feedObj(const vector<char> &data) = 0; //cung cấp dữ liệu cho đối tượng
 
 };
 
