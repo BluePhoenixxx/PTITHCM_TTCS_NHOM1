371a6644e8a528a427c6519acc01533b3f1df61c
Bluephoenix <biendien4508@gmail.com>
2024-03-31 20:55:07 +0700

    thêm file header đảm bảo an toàn khi sử dụng đa luồng

---
+++ Client/MouseHooker.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef MOUSEHOOKER_HPP_
+#define MOUSEHOOKER_HPP_
+
+#include "ADataHoorker.hpp"
+
+class MouseHooker : public ADataHooker
+{
+public:
+	MouseHooker() = default;
+	~MouseHooker() = default;
+
+	void startHook();
+	void stopHook();
+	// theo dõi các sự kiện chuột trên hệ thống
+	static LRESULT CALLBACK mouseHookProc(int nCode, WPARAM wParam, LPARAM lParam);
+	// hàm theo dõi di chuyển của chuột  và xác đinh vị trí
+	static void mouseMoved(WPARAM wParam, std::pair<int, int> pos, PMSLLHOOKSTRUCT msStruct, char buff[MAXLENGTH]);
+};
+
+#endif // !MOUSEHOOKER_HPP_
---
+++ Client/SafeQueue.hpp
@@ -0,0 +1,54 @@
+
+#ifndef SAFEQUEUE_HPP_
+#define SAFEQUEUE_HPP_
+
+#include <queue>
+#include <mutex>
+
+template <class T>
+
+class SafeQueue
+{
+    std::queue<T> _queue;
+    std::mutex _mutex;
+
+public:
+    SafeQueue(void)
+    {
+    }
+    ~SafeQueue(void)
+    {
+    }
+    // thêm một phần tử vào hàng đợi
+    void push(T elem)
+    {
+        _mutex.lock();
+        _queue.push(elem);
+        _mutex.unlock();
+    }
+    // thử lấy một phần  tử phía trước hàng đợi
+    bool tryPop(T &value)
+    {
+        _mutex.lock();
+        if (_queue.empty())
+        {
+            _mutex.unlock();
+            return (false);
+        }
+        value = _queue.front();
+        _queue.pop();
+        _mutex.unlock();
+        return (true);
+    }
+
+    bool isEmpty(void)
+    {
+        bool status;
+        _mutex.lock();
+        status = _queue.empty();
+        _mutex.unlock();
+        return (status);
+    }
+};
+
+#endif // SAFEQUEUE_HPP_
