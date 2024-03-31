7ccf7407d1c57b08a08d661d947285cec976e1e9
Bluephoenix <biendien4508@gmail.com>
2024-03-31 14:57:21 +0700

    Tạo file header ghi IInput ghi nhận tham số ở máy clinet

--- Client/IInput.hpp
+++ Client/IInput.hpp
@@ -1,17 +1,16 @@
-#pragma once
-
 #ifndef IINPUT_HPP_
-# define IINPUT_HPP_
+#define IINPUT_HPP_
 
 #include <utility>
 
-class								IInput
+class IInput
 {
 public:
-	virtual int						getKeyInput() = 0;
-	virtual std::pair<int, int>		getMouseInput() = 0;
-	virtual int						getTime() = 0;
-	virtual char					*getProcessus() = 0;
+    virtual int getKeyInput() = 0;
+    virtual std::pair<int, int> getMouseInput() = 0;
+    virtual int getTime() = 0;
+    virtual char *getProcessus() = 0;
+    // trả về con trỏ đến một chuỗi ký tự địa diện cho tên tiến trình
 };
 
 #endif // !IINPUT_HPP_
